34a9e3c1935b1fff8715a40cb6770934aff565f1
Vlad Bereznitskiy <sttvld@gmail.com>
2016-04-17 15:40:25 +0300

    HomeView: reformat

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,8 +1,7 @@
 /* @flow */
-import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { increment, doubleAsync } from '../../redux/modules/counter'
-import DuckImage from './Duck.jpg'
+import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {increment, doubleAsync} from '../../redux/modules/counter'
 import classes from './HomeView.scss'
 
 // We can use Flow (http://flowtype.org/) to type our component's props
@@ -13,53 +12,52 @@ import classes from './HomeView.scss'
 // code, or `npm i -g flow-bin` to have access to the binary globally.
 // Sorry Windows users :(.
 type Props = {
-  counter: number,
-  doubleAsync: Function,
-  increment: Function
+    counter: number,
+    doubleAsync: Function,
+    increment: Function
 };
 
 // We avoid using the `@connect` decorator on the class definition so
 // that we can export the undecorated component for testing.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
 export class HomeView extends React.Component<void, Props, void> {
-  static propTypes = {
-    counter: PropTypes.number.isRequired,
-    doubleAsync: PropTypes.func.isRequired,
-    increment: PropTypes.func.isRequired
-  };
+    static propTypes = {
+        counter: PropTypes.number.isRequired,
+        doubleAsync: PropTypes.func.isRequired,
+        increment: PropTypes.func.isRequired
+    };
 
-  render () {
-    return (
-      <div className='container text-center'>
-        <div className='row'>
-          <div className='col-xs-2 col-xs-offset-5'>
-            <img className={classes.duck}
-              src={DuckImage}
-              alt='This is a duck, because Redux.' />
-          </div>
-        </div>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>
-          Sample Counter:
-          {' '}
-          <span className={classes['counter--green']}>{this.props.counter}</span>
-        </h2>
-        <button className='btn btn-default' onClick={this.props.increment}>
-          Increment
-        </button>
-        {' '}
-        <button className='btn btn-default' onClick={this.props.doubleAsync}>
-          Double (Async)
-        </button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container text-center'>
+                <div className='row'>
+                    <div className='col-xs-2 col-xs-offset-5'>
+                        <img className={classes.duck}
+                             alt='This is a duck, because Redux.'/>
+                    </div>
+                </div>
+                <h1>Welcome to the React Redux Starter Kit</h1>
+                <h2>
+                    Sample Counter:
+                    {' '}
+                    <span className={classes['counter--green']}>{this.props.counter}</span>
+                </h2>
+                <button className='btn btn-default' onClick={this.props.increment}>
+                    Increment
+                </button>
+                {' '}
+                <button className='btn btn-default' onClick={this.props.doubleAsync}>
+                    Double (Async)
+                </button>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  counter: state.counter
+    counter: state.counter
 })
 export default connect((mapStateToProps), {
-  increment: () => increment(1),
-  doubleAsync
+    increment: () => increment(1),
+    doubleAsync
 })(HomeView)
