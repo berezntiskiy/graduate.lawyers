ca7b89b9db2431bbe63c8ca2a979142cb5aee417
Vlad Bereznitskiy <sttvld@gmail.com>
2016-06-14 01:34:25 +0300

    добавление в избранное

--- frontend/src/app/library/article-list.component.ts
+++ frontend/src/app/library/article-list.component.ts
@@ -11,6 +11,7 @@ import {SectionService} from "./section.service";
 import {Section} from "./section";
 import {ChapterService} from "./chapter.service";
 import {Chapter} from "./chapter";
+import {SessionService} from "../user/session.service";
 
 @Component({
     selector: 'article-list',
@@ -33,9 +34,15 @@ import {Chapter} from "./chapter";
         </div>
         <div *ngIf="!isLoading">
             <books-sidebar-list>
-                <books-sidebar-item (click)="emmitOpen(entity)" title="{{entity.title}}"
-                                    description="{{entity.text}}"
-                                    *ngFor="let entity of collection"></books-sidebar-item>
+                <books-sidebar-item
+                    (like)="like.emit({value: entity})"
+                    (unlike)="unlike.emit({value: entity})"
+                    [canLike]="SessionService.auth"
+                     
+                    (click)="emmitOpen(entity)" title="{{entity.title}}"
+                    description="{{entity.text}}"
+                    likes="{{entity.likes}}"
+                    *ngFor="let entity of collection"></books-sidebar-item>
                 <div class="no-items" *ngIf="collection && collection.length == 0">No items</div>
             </books-sidebar-list>
         </div>
@@ -45,6 +52,9 @@ export class ArticleList {
     @Input() isLoading:boolean;
     @Input() collection:any[];
     @Output() open: EventEmitter<any> = new EventEmitter();
+    @Output() like:EventEmitter<any> = new EventEmitter();
+    @Output() unlike:EventEmitter<any> = new EventEmitter();
+    private SessionService = SessionService;
 
     emmitOpen(entity) {
         this.open.emit({value: entity});
