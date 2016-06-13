14c0140aa92a1c3c98080ea14d740685763edfbb
Vlad Bereznitskiy <sttvld@gmail.com>
2016-06-13 21:56:18 +0300

    contact

--- frontend/src/app/contact/contact-letter.component.ts
+++ frontend/src/app/contact/contact-letter.component.ts
@@ -1,33 +1,56 @@
 import {Component, OnInit, Input, Output} from '@angular/core';
 import {EventEmitter} from "@angular/compiler/src/facade/async";
 import {FormBuilder, Validators, Control} from "@angular/common";
+import {ContactService} from "./contact.service";
 
+enum Status {
+    success,
+    failed
+}
 
 @Component({
     selector: 'contact-letter',
-    providers: [],
+    providers: [
+        ContactService
+    ],
     viewProviders: [],
     directives: [],
     pipes: [],
+    styles: [`
+      .bad {
+        color: tomato;
+        margin: 5px 0 15px;
+      }
+`],
     template: `
-    <form [ngFormModel]="form" (submit)="emitSend()">
+    <form [ngFormModel]="form" (submit)="send()">
         <md-input style="width:100%" placeholder="Name" required type="text" ngControl="name" ></md-input>
         <md-input style="width:100%" placeholder="Email" required type="text" ngControl="email" ></md-input>
         <md-input style="width:100%" placeholder="Phone" required type="text" ngControl="phone" ></md-input>
         <md-input style="width:100%" placeholder="Message" required type="textarea" ngControl="body" ></md-input>
         
-        <button md-raised-button color="primary" type="submit">Send</button>
+        <div class="bad" [ngSwitch]="status">
+            <div *ngSwitchWhen="statuses.success">
+                We will reply as soon as possible.
+            </div>
+            <div *ngSwitchWhen="statuses.failed">
+                Something went wrong, please try again.
+            </div>
+        </div>
+        
+        <button md-raised-button color="primary" [disabled]="sending" type="submit">Send</button>
     </form>
 `
 })
 export class ContactLetter implements OnInit {
     @Input() private conversations = null;
     @Input() private activeConversation = null;
-    @Output() send: EventEmitter<any> = new EventEmitter();
-
+    private status:Status;
+    private statuses:Status = Status;
+    private sending = false;
     form:any;
 
-    constructor(private builder: FormBuilder) {
+    constructor(private builder:FormBuilder, private contactService:ContactService) {
         const name = new Control('', Validators.required);
         const email = new Control('', Validators.required);
         const phone = new Control('', Validators.required);
@@ -44,8 +67,16 @@ export class ContactLetter implements OnInit {
     ngOnInit() {
     }
 
-    emitSend() {
-        this.send.emit({value: this.form.value});
-        (<Control>this.form.controls['body']).updateValue('');
+    send() {
+        this.status = null;
+        this.sending = true;
+        this.contactService.send(this.form.value).subscribe(() => {
+            (<Control>this.form.controls['body']).updateValue('');
+            this.status = Status.success;
+            this.sending = false;
+        }, () => {
+            this.status = Status.failed;
+            this.sending = false;
+        });
     }
 }
---
+++ frontend/src/app/contact/contact.service.ts
@@ -0,0 +1,19 @@
+import {Injectable, Inject} from '@angular/core';
+import {Http, Response, Request} from '@angular/http';
+
+import {RESTClient, GET, PUT, POST, DELETE, BaseUrl, Headers, DefaultHeaders, Path, Body, Query} from 'core/angular2-rest.ts';
+import {Observable} from "rxjs/Observable";
+import {CoreClient} from "../../core/CoreConnector";
+import {Contact} from "./contact";
+
+
+export class ContactService extends CoreClient {
+
+    public constructor( @Inject(Http) protected http: Http) {
+        super(http);
+    }
+
+    @POST("contact")
+    public send( @Body contact: Contact): Observable { return null; };
+
+}
